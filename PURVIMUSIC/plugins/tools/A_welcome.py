04cbf095cc11290ab597abcac8848be437360773
ME KALIN BHAIYA <mdistkhar1078@gmail.com>
2024-07-17 20:10:37 +0530

    Update A_welcome.py

--- PURVIMUSIC/plugins/tools/A_welcome.py
+++ PURVIMUSIC/plugins/tools/A_welcome.py
@@ -20,6 +20,7 @@ from pyrogram.enums import ParseMode
 from pyrogram import *
 from pyrogram.types import *
 from logging import getLogger
+from PURVIMUSIC.utils.purvi_ban import admin_filter
 import os
 from PIL import ImageDraw, Image, ImageFont, ImageChops
 from pyrogram import *

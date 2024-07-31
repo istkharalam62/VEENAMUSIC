adc4262065fea7c8d1b377bceafc433eb6aded3e
TEAM ISTKHAR <mdistkhar1078@gmail.com>
2024-07-31 11:47:02 +0530

    Update couples.py

--- PURVIMUSIC/plugins/tools/couples.py
+++ PURVIMUSIC/plugins/tools/couples.py
@@ -14,7 +14,7 @@ from PURVIMUSIC.mongo.couples_db import _get_image, get_couple
 POLICE = [
     [
         InlineKeyboardButton(
-            text="|| ˹𖤍 𝗔𝗟𝗣𝗛𝗔 𖤍˼ ||",
+            text="|| ˹𖤍 𝗜𝗦𝗧𝗞𝗛𝗔𝗥 𖤍˼ ||",
             url=f"https://t.me/PURVI_UPDATES",
         ),
     ],
